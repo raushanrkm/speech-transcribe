6291ba20929a9ddd81a6118930537df7e62a22a5
Raushan Mahaseth <raushanrkm@Raushans-MacBook-Air.local>
2024-04-14 12:08:20 -0700

    add transcribe

--- main.py
+++ main.py
@@ -1,44 +1,96 @@
-import moviepy.editor as mp
-import speech_recognition as sr
-from fastapi import FastAPI, Form, Request, File
-from fastapi.responses import HTMLResponse
-from fastapi.templating import Jinja2Templates
-from fastapi import UploadFile
-import os
+import time
+import math
+import ffmpeg
 
-app = FastAPI()
-templates = Jinja2Templates(directory='templates')
+from faster_whisper import WhisperModel
+
+input_video = "input.mp4"
+input_video_name = input_video.replace(".mp4", "")
 
 
-@app.post('/upload')
-def uploadMp4_video(file: UploadFile = File(...)):
-    file_name = file.filename
-    mime_type = file.content_type
-    file_data = file.file
-    temp_filepath = "temp.mp4"
+def extract_audio():
+    extracted_audio = f"audio-{input_video_name}.wav"
+    stream = ffmpeg.input(input_video)
+    stream = ffmpeg.output(stream, extracted_audio)
+    ffmpeg.run(stream, overwrite_output=True)
+    return extracted_audio
 
-    print(f'file name :{file_name} and mime_type : {mime_type}')
-    with open(temp_filepath, "wb") as f:
-        f.write(file_data.read())
-
-    video = mp.VideoFileClip(temp_filepath)
-    audio_file = video.audio
-    audio_file.write_audiofile("temp.wav")
-    r = sr.Recognizer()
-    with sr.AudioFile("temp.wav") as source:
-        data = r.record(source)
-        text = r.recognize_google(data)
-
-    os.remove(temp_filepath)
-    os.remove("temp.wav")
-    return text
+def transcribe(audio):
+    model = WhisperModel("small")
+    segments, info = model.transcribe(audio)
+    language = info[0]
+    print("Transcription language", info[0])
+    segments = list(segments)
+    for segment in segments:
+        # print(segment)
+        print("[%.2fs -> %.2fs] %s" %
+              (segment.start, segment.end, segment.text))
+    return language, segments
 
 
-@app.get('/', response_class=HTMLResponse)
-def main(request: Request):
-    return templates.TemplateResponse('index.html', {'request': request})
+def format_time(seconds):
+
+    hours = math.floor(seconds / 3600)
+    seconds %= 3600
+    minutes = math.floor(seconds / 60)
+    seconds %= 60
+    milliseconds = round((seconds - math.floor(seconds)) * 1000)
+    seconds = math.floor(seconds)
+    formatted_time = f"{hours:02d}:{minutes:02d}:{seconds:01d},{milliseconds:03d}"
+
+    return formatted_time
 
 
-@app.post('/test')
-def test(request: Request):
-    return templates.TemplateResponse('index.html', {'request': request})
+def generate_subtitle_file(language, segments):
+    subtitle_file = f"sub-{input_video_name}.{language}.srt"
+    text = ""
+    for index, segment in enumerate(segments):
+        segment_start = format_time(segment.start)
+        segment_end = format_time(segment.end)
+        text += f"{str(index + 1)} \n"
+        text += f"{segment_start} --> {segment_end} \n"
+        text += f"{segment.text} \n"
+        text += "\n"
+
+    f = open(subtitle_file, "w")
+    f.write(text)
+    f.close()
+
+    return subtitle_file
+
+def add_subtitle_to_video(soft_subtitle, subtitle_file,  subtitle_language):
+
+    video_input_stream = ffmpeg.input(input_video)
+    subtitle_input_stream = ffmpeg.input(subtitle_file)
+    output_video = f"output-{input_video_name}.mp4"
+    subtitle_track_title = subtitle_file.replace(".srt", "")
+
+    if soft_subtitle:
+        stream = ffmpeg.output(
+            video_input_stream, subtitle_input_stream, output_video, **{"c": "copy", "c:s": "mov_text"},
+            **{"metadata:s:s:0": f"language={subtitle_language}",
+            "metadata:s:s:0": f"title={subtitle_track_title}"}
+        )
+        ffmpeg.run(stream, overwrite_output=True)
+    else:
+        stream = ffmpeg.output(video_input_stream, output_video,
+
+                               vf=f"subtitles={subtitle_file}")
+
+        ffmpeg.run(stream, overwrite_output=True)
+
+
+def run():
+    extracted_audio = extract_audio()
+    language, segments = transcribe(audio=extracted_audio)
+    subtitle_file = generate_subtitle_file(
+        language=language,
+        segments=segments
+    )
+
+    add_subtitle_to_video(
+        soft_subtitle=True,
+        subtitle_file=subtitle_file,
+        subtitle_language=language
+    )
+run()
